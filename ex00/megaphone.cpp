adf6067d51a12df8f630907c1b6f840e1baaa42b
root <root@DESKTOP-B2T6I56>
2023-03-07 16:51:57 +0100

    ex00 done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc < 2) {
+		std::cout << "* LOUD AND UNEARABLE NOISE *\n";
+		return(0);
+	}
+	i = 1;
+	while (argv[i]) {
+		j = 0;
+		while (argv[i][j])
+		{
+			std::cout << (char)std::toupper(argv[i][j]);
+			j++;
+		}
+		i++;
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+}
